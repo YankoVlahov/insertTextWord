ce301b4b0d62b1c17649f8ddfbcacdad7a56126f
Yanko Vlahov <qnko.vlahov39@gmail.com>
2022-09-21 09:43:34 +0300

    Add project files.

---
+++ insertTextWrod/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace insertTextWrod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "load file and preview";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "runtime word";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(591, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "load file and replace ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(577, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(577, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(577, 157);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(527, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(518, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Position";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(500, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Department";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(500, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "From_date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(577, 203);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(162, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ insertTextWrod/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Words;
+using Microsoft.Office.Interop.Word;
+using word= Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Threading;
+
+namespace insertTextWrod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Microsoft.Office.Interop.Word.Application winword = new Microsoft.Office.Interop.Word.Application();
+            using (OpenFileDialog wordRead = new OpenFileDialog())
+
+            {
+                if (wordRead.ShowDialog() == DialogResult.OK)
+                {
+
+                    object file = wordRead.FileName;
+
+                    //MessageBox.Show(file);
+                    word.Application wordAPp = new word.Application();
+                    //  word.Document aDoc = null;
+                    object missing = System.Reflection.Missing.Value;
+                    wordAPp.Visible = true;
+                    Microsoft.Office.Interop.Word.Document docs = wordAPp.Documents.Open
+                    (ref file, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing);
+
+
+                    // wordRead.OpenFile();
+
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CreateDocument();
+        }
+
+        private void CreateDocument()
+        {
+            try
+            {
+                //Create an instance for word app  
+                Microsoft.Office.Interop.Word.Application winword = new Microsoft.Office.Interop.Word.Application();
+
+                //Set animation status for word application  
+                winword.ShowAnimation = false;
+
+
+                winword.Visible = false;
+
+
+                object missing = System.Reflection.Missing.Value;
+
+
+                Microsoft.Office.Interop.Word.Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+
+
+                // Save the document
+                object filename = @"D:\temp2.doc";
+                document.SaveAs2(ref filename);
+                document.Close(ref missing, ref missing, ref missing);
+                document = null;
+                winword.Quit(ref missing, ref missing, ref missing);
+                winword = null;
+                MessageBox.Show("Document created successfully !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        public partial class SqlDb
+        {
+            
+            public string Ip { get; set; }
+            public string Name { get; set; }
+            public string Position { get; set; }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //brow на word
+            
+
+                   
+
+                    List<SqlDb> filTheText = new List<SqlDb>();
+                    var data = new SqlDb { Ip = textBox1.Text, Name = textBox2.Text, Position = "чистач на кенефи" };
+                    filTheText.Add(data);
+
+                    //string name = textBox1.Text;
+                    //string position = textBox2.Text;
+                    //string department = textBox3.Text;
+                    //string from_date = dateTimePicker1.Text;
+
+                    ExportWord(filTheText);
+        }
+
+        private void ExportWord(List<SqlDb> filTheText)
+        {
+            using (OpenFileDialog wordRead = new OpenFileDialog())
+
+            {
+                if (wordRead.ShowDialog() == DialogResult.OK)
+                {
+                    object file = wordRead.FileName;
+                    //word.Application wordAPp = new word.Application();
+                    object missing = System.Reflection.Missing.Value;
+                    object readOnly = false;
+                    var fileName = Path.Combine(System.Windows.Forms.Application.StartupPath, wordRead.FileName);
+                    Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application { Visible = false };
+                    Microsoft.Office.Interop.Word.Document aDoc = wordApp.Documents.Open(fileName, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+                    // Microsoft.Office.Interop.Word.Document aDoc = wordApp.Documents.Open();
+                    foreach (var item in filTheText)
+                    {
+                        foreach (var prop in item.GetType().GetProperties())
+                        {
+
+                            FindAndReplace(wordApp, prop.Name, prop.GetValue(item, null));
+                        }
+                    }
+                    //aDoc.Activate();
+                    wordApp.Visible = true;
+                }
+            }
+        }
+
+        private void FindAndReplace(Microsoft.Office.Interop.Word.Application fileOpen , object findText, object replaceWithText)
+                {
+                    object matchCase = false;
+                    object matchWholeWord = true;
+                    object matchWildCards = false;
+                    object matchSoundsLike = false;
+                    object matchAllWordForms = false;
+                    object forward = true;
+                    object format = false;
+                    object matchKashida = false;
+                    object matchDiacritics = false;
+                    object matchAlefHamza = false;
+                    object matchControl = false;
+                    object read_only = false;
+                    object visible = false;
+                    object replace = 2;
+                    object wrap = 1;
+            //find and replace 
+            fileOpen.Selection.SetRange(fileOpen.ActiveDocument.Content.Start, fileOpen.ActiveDocument.Content.End);
+                    fileOpen.Selection.Find.Execute(ref findText, ref matchCase, ref matchWholeWord,
+                        ref matchWildCards, ref matchSoundsLike, ref matchAllWordForms, ref forward, ref wrap, ref format, ref replaceWithText, ref replace,
+                        ref matchKashida, ref matchDiacritics, ref matchAlefHamza, ref matchControl);
+                }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime dt1 = dateTimePicker1.Value;
+            string theDate = dateTimePicker1.Value.ToString("dd.MM.yyyy");
+           // MessageBox.Show(theDate);
+
+        }
+    }
+}
+    
+
+    
+    
+
+
